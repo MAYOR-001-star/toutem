e2269e85c8a85f828e2860b73a722b559dae5e85
MAYOR-001-star <mayorkunabdulazeez01@gmail.com>
2026-03-27 14:55:12 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-500 underline">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
